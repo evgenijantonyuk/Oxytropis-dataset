a5d8ea8941ca961725a415904170f9a976a7c0cb
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-11-27 22:52:56 +0700

    finished list

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    // ======== Gallery ========
+    new Gallery(document.getElementById('gallery'), {
+        margin: 15
+    })
+//     =========================
+})
+                
+
+    
