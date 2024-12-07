1be303dccab58da9c9f6fdf49ecfb2508f3eb4cf
Евгений Антонюк <evgenijantonyuk@gmail.com>
2024-12-07 21:37:48 +0700

    add list

---
+++ js/list.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //     ======== Finding ============
+    document.querySelector('#input').oninput = function () {
+        let val = this.value.trim().toLowerCase()
+        let searchItems = document.querySelectorAll('.search')
+        if (val !== '') {
+            searchItems.forEach(function (elem) {
+                if (elem.innerText.search(val) === -1) {
+                    elem.classList.add('hide')
+                    elem.innerHTML = elem.innerText
+                } else {
+                    elem.classList.remove('hide')
+                    let str = elem.innerText
+                    elem.innerHTML = insertMark(str, elem.innerText.search(val), val.length)
+                }
+            })
+        } else {
+            searchItems.forEach(function (elem) {
+                elem.classList.remove('hide')
+                elem.innerHTML = elem.innerText
+            })
+        }
+    }
+    
+    function insertMark(symbols, position, len) {
+        return symbols.slice(0, position) + '<mark>' + symbols.slice(position, position + len) + '</mark>' + symbols.slice(position + len)
+    }
+})
