987ada887947f4f663be74a42ee0d0dd193ef1f7
Евгений Антонюк <evgenijantonyuk@gmail.com>
2025-01-24 23:52:35 +0700

    add list and key

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ document.addEventListener('DOMContentLoaded', function(){
         margin: 15
     })
 //     =========================
-})
-                
-
     
+    const buttonShow = document.querySelector('.contacts-button')
+    const contacts = document.querySelector('.contacts')
+    const container = document.querySelector('.container')
+    
+    buttonShow.addEventListener('click', function(){
+        contacts.classList.toggle('active');
+        container.classList.toggle('active');
+    })
+
+})
+
+
+
