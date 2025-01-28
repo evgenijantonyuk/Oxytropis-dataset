d5ef9ac554e42ec0a8c0a0f73b603e353220958f
Евгений Антонюк <evgenijantonyuk@gmail.com>
2025-01-28 23:34:18 +0700

    добавлена форма

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     new Gallery(document.getElementById('gallery'), {
         margin: 15
     })
-//     =========================
+//     ====================================== modal ===================================
     
     const buttonShow = document.querySelector('.contacts-button')
     const contacts = document.querySelector('.contacts')
@@ -13,8 +13,96 @@ document.addEventListener('DOMContentLoaded', function(){
         contacts.classList.toggle('active');
         container.classList.toggle('active');
     })
-
 })
+
+
+// ======================================== form ======================================
+const form = document.forms["form"];
+const formArr = Array.from(form);
+const validFormArr = [];
+const button = form.elements["button"];
+
+formArr.forEach((el) => {
+    if (el.hasAttribute("data-reg")) {
+        el.setAttribute("is-valid", "0");
+        validFormArr.push(el);
+    }
+});
+
+form.addEventListener("input", inputHandler);
+form.addEventListener("submit", formCheck);
+
+function inputHandler({ target }) {
+    if (target.hasAttribute("data-reg")) {
+        inputCheck(target);
+    }
+}
+
+function inputCheck(el) {
+    const inputValue = el.value;
+    const inputReg = el.getAttribute("data-reg");
+    const reg = new RegExp(inputReg);
+    if (reg.test(inputValue)) {
+        el.setAttribute("is-valid", "1");
+        el.style.border = "2px solid rgb(0, 196, 0)";
+    } else {
+        el.setAttribute("is-valid", "0");
+        el.style.border = "2px solid rgb(255, 0, 0)";
+    }
+}
+
+function buttonHandler(e) {
+    const isAllValid = [];
+}
+
+function formCheck(e) {
+    e.preventDefault();
+    const allValid = [];
+    validFormArr.forEach((el) => {
+        allValid.push(el.getAttribute("is-valid"));
+    });
+    const isAllValid = allValid.reduce((acc, current) => {
+        return acc && current;
+    });
+    if (!Boolean(Number(isAllValid))) {
+        alert("Заполните поля правильно!");
+        return;
+    }
+    formSubmit();
+}
+
+async function formSubmit() {
+    const data = serializeForm(form);
+    const response = await sendData(data);
+    if (response.ok) {
+        let result = await response.json();
+        alert(result.message);
+        formReset();
+    } else {
+        alert("Код ошибки: " + response.status);
+    }
+}
+
+function serializeForm(formNode) {
+    return new FormData(form);
+}
+
+async function sendData(data) {
+    return await fetch("send_mail.php", {
+        method: "POST",
+        body: data,
+    });
+}
+
+function formReset() {
+    form.reset();
+    validFormArr.forEach((el) => {
+        el.setAttribute("is-valid", 0);
+        el.style.border = "none";
+    });
+}
+
+// ================================================================================================
 
 
 
