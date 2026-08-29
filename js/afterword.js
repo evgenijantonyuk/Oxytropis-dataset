8ba1bdfba57b5fd776b3d71c723b04fda1be93ed
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-08-29 20:09:09 +0700

    fixed JS and CSS

--- js/afterword.js
+++ js/afterword.js
@@ -9,26 +9,4 @@ burgerOpenMenuBtn.addEventListener('click', function () {
     burgerCloseMenu.classList.toggle('active')
 })
 
-const buttonTop = document.querySelector('.button__up')
-
-const showButton = () => {
-    window.addEventListener('scroll', () => {
-
-        if (window.scrollY > 100) {
-            buttonTop.classList.add('show')
-        } else {
-            buttonTop.classList.remove('show')
-        }
-    })
-}
-
-const scrollToTop = () => {
-    buttonTop.addEventListener('click', () => {
-        window.scroll({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        })
-    })
-}
 
